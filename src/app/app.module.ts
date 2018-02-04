6226d74ce68308960368c6a25c2964a1382b0e94
Cem KAYA <ahmetcem3@gmail.com>
2018-02-03 21:16:59 -0600

    google maps api integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-
 import { AppComponent } from './app.component';
+import { AgmCoreModule } from '@agm/core';
 
 
 @NgModule({
@@ -10,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,NgbModule.forRoot()
+    BrowserModule, NgbModule.forRoot(), AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBJC0apf3_BgbDffq-XzpOqehwujK9sFho'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
