ce393358be447a2a60d9ff58a3ff80914ea006ba
muhammadelsayed <msayed@dualparadox.com>
2018-02-04 23:58:57 -0600

    Adding common modules, services,.. and login screen.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,60 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes} from '@angular/router';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import { AppComponent } from './app.component';
 import { AgmCoreModule } from '@agm/core';
+import {ToastModule} from 'ng2-toastr/ng2-toastr';
+import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { RouterModule, Routes} from '@angular/router';
-import { WebService } from './services/web.service';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { LoginComponent } from './account/login/login.component';
+import { RegisterComponent } from './account/register/register.component';
+import { AdminComponent } from './account/admin/admin.component';
+import { WebService, AuthenticationService, UserService } from './services/all';
+import { AuthGuard } from './guards/auth.guard';
+import { AdminGuard } from './guards/admin.guard';
+import { JwtInterceptor } from './interceptors/jwt.interceptor';
+import { ErrorInterceptor } from './interceptors/error.interceptor';
 
 const ROUTES = [
   {path: '' , redirectTo: 'home', pathMatch: 'full'},
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  { path: 'admin', component: AdminComponent, canActivate: [AuthGuard, AdminGuard] },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     NgbModule.forRoot(),
     AgmCoreModule.forRoot({apiKey: 'AIzaSyBJC0apf3_BgbDffq-XzpOqehwujK9sFho', libraries: ['geometry', 'places']}),
     RouterModule.forRoot(ROUTES),
+    HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    ToastModule.forRoot()
   ],
-  providers: [WebService],
+  providers: [
+    WebService,
+    AuthGuard,
+    AuthenticationService,
+    UserService,
+    JwtInterceptor,
+    ErrorInterceptor
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
