5c4e2815aca53210cbcfdde7aac220ba1c8a3e9d
muhammadelsayed <msayed@dualparadox.com>
2018-02-06 00:43:12 -0600

    update the user services and it's usage.

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -8,23 +8,17 @@ import { AppConfig } from '../app.config';
 export class AuthenticationService {
     constructor(private http: HttpClient) { }
 
-    public currentUser:any;
     login(email: string, password: string) {
         return this.http.post<any>(AppConfig.apiUrl + '/users/auth', { email: email, password: password })
             .map(user => {
                 // login successful if there's a jwt token in the response
                 if (user && user.token) {
                     // store user details and jwt token in local storage to keep user logged in between page refreshes
-                    this.currentUser = user;
                     localStorage.setItem('currentUser', JSON.stringify(user));
                 }
 
                 return user;
             });
-    }
-
-    getCurrentUser(){
-        return this.currentUser;
     }
 
     logout() {
