3618872596c0204c4765379549df323abe654d68
gaosh <gaosh@172.19.142.215>
2018-02-05 22:43:16 -0600

    finish group CRUD component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,29 @@ import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { AgmCoreModule } from '@agm/core';
+import { HttpClientModule} from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule, Routes} from '@angular/router';
 import { WebService } from './services/web.service';
+import { GroupService } from './services/group.service';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { GroupComponent } from './group/group.component';
+import {  FormBuilder } from "@angular/forms";
+import { NguiDatetimePickerModule } from '@ngui/datetime-picker';
 
 const ROUTES = [
   {path: '' , redirectTo: 'home', pathMatch: 'full'},
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent}, 
+  {path: 'admin', component: GroupComponent}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    GroupComponent
   ],
   imports: [
     BrowserModule,
@@ -26,9 +33,10 @@ const ROUTES = [
     AgmCoreModule.forRoot({apiKey: 'AIzaSyBJC0apf3_BgbDffq-XzpOqehwujK9sFho', libraries: ['geometry', 'places']}),
     RouterModule.forRoot(ROUTES),
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
-  providers: [WebService],
+  providers: [FormBuilder, WebService, GroupService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
