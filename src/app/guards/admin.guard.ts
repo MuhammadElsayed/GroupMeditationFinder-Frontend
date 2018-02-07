0607d79999fc5edf2438e2a142b3c735977a841b
muhammadelsayed <msayed@dualparadox.com>
2018-02-06 18:11:01 -0600

    update guards.

--- src/app/guards/admin.guard.ts
+++ src/app/guards/admin.guard.ts
@@ -1,14 +1,17 @@
 ﻿import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { UserService } from '../services/user.service';
 
 @Injectable()
 export class AdminGuard implements CanActivate {
 
-    constructor(private router: Router) { }
+    constructor(
+      private router: Router,
+      private userService: UserService
+    ) { }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        if (localStorage.getItem('currentUser')) {
-            // @TODO: check if the user role is admin
+        if (this.userService.isAdmin()) {// check if the user role is admin
             return true;
         }
 
